53523abec28906de749d6e102eb609da7ed0e70a
Lcfvs <lcf.vs@hotmail.com>
2013-03-28 18:32:41 +0100

    Update sandbox.js
    Catchable sandbox
    New onreadystatechange "event" (replaces the callback system)
    
    Compatibility :
    IE7+
    Firefox 19 (& certainly a lot of previous versions)
    Chrome 25 (& certainly a lot of previous versions)

--- sandbox.js
+++ sandbox.js
@@ -1,27 +1,63 @@
 'use strict';
 var Sandbox;
-Sandbox = function Sandbox(src, callbackName) {
-    var fragment, iframe;
-    fragment = document.createDocumentFragment();
-    iframe = document.createElement('iframe');
-    iframe.style.display = 'none';
-    iframe.name = 'Sandbox__' + ((callbackName || '') + new Date().getTime()) + Math.floor(Math.random() * 1000);
-    iframe.onload = function(){
-        var sandbox, document, script, callback;
-        sandbox = frames[iframe.name];
-        document = sandbox.document;
-        script = document.createElement('script');
-        script.src = src;
-        script.onload = function(){
-            if(typeof callbackName === 'string') {
-                callback = sandbox[callbackName];
-            }
-            fragment.appendChild(iframe);
-            if(typeof callback === 'function') {
-                callback(window);
-            }
-        };
-        document.body.appendChild(script);
+Sandbox = (function(window) {
+    var Sandbox, queue, loadSandbox, readyStateChange;
+    queue = [];
+    Sandbox = function Sandbox(src) {
+        this.src = src;
+        this.window = void null;
+        this.readyState = -1;
+        queue.push(this);
+        readyStateChange(this);
+        if(queue.length === 1 && queue[0].readyState === 0) {
+            loadSandbox();
+        }
     };
-    document.body.appendChild(iframe);
-};
+    readyStateChange = function readyStateChange(instance) {
+        instance.readyState += 1;
+        if(typeof instance.onreadystatechange === 'function') {
+            instance.onreadystatechange();
+        }
+    };
+    loadSandbox = function loadSandbox() {
+        var instance, fragment, iframe, src;
+        instance = queue[0];
+        readyStateChange(instance);
+        fragment = document.createDocumentFragment();
+        iframe = document.createElement('iframe');
+        src = instance.src;
+        iframe.style.display = 'none';
+        iframe.id = iframe.name = ('Sandbox__' + new Date().getTime()) + Math.floor(Math.random() * 1000000000000000);
+        iframe.onload = function() {
+            var sandbox, document, script, onload, Window;
+            Window = function Window(window) {
+                if(!this.document) {
+                    return window;
+                }
+            };
+            Window.prototype = frames[iframe.name] || window.document.getelementById(iframe.id).contentWindow;
+            instance.window = sandbox = new Window(window);
+            document = sandbox.document;
+            script = document.createElement('script');
+            script.src = src;
+            onload = function() {
+                var readyState;
+                readyState = this.readyState;
+                if(typeof readyState !== 'string' || (readyState === 'loaded' || readyState === 'complete')) {
+                    fragment.appendChild(iframe);
+                    readyStateChange(instance);
+                    queue.shift();
+                    if(queue.length !== 0) {
+                        loadSandbox();
+                    }
+                }
+            };
+            script[script.readyState ? 'onreadystatechange' : 'onload'] = onload;
+            document.body.appendChild(script);
+            readyStateChange(instance);
+        };
+        document.body.appendChild(iframe);
+        readyStateChange(instance);
+    };
+    return Sandbox;
+})(window);
