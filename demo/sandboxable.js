8a7f04dc13fadda54925808a8974dfae9537129c
Lcfvs <lcf.vs@hotmail.com>
2013-03-28 18:24:16 +0100

    Update sandboxable.js
    Appends the button in a P tag

--- demo/sandboxable.js
+++ demo/sandboxable.js
@@ -10,3 +10,15 @@
     }, false);
     parentWindow.document.body.getElementsByTagName('p')[0].appendChild(button);
 })(parent.window || window);
+'use strict';
+(function(parentWindow) {
+    var doc, button;
+    doc = window.document;
+    button = doc.createElement('button');
+    button.innerHTML = 'Click here';
+    button.setAttribute('type', 'button');
+    button.onclick = function() {
+        parentWindow.alert('This button has been created from an anonymous JavaScript sandbox');
+    };
+    parentWindow.document.body.getElementsByTagName('p')[0].appendChild(button);
+})(parent.window || window);
