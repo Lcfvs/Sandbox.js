ecc0f0eb9f012eadde9ccf31f3facf9f8065e9fa
Lcfvs <lcf.vs@hotmail.com>
2013-03-28 21:08:27 +0100

    Update sandboxable.js

--- demo/sandboxable.js
+++ demo/sandboxable.js
@@ -1,20 +1,7 @@
 'use strict';
 (function(parentWindow) {
-    var doc, button;
-    doc = window.document;
-    button = doc.createElement('button');
-    button.innerHTML = 'Click here';
-    button.type = 'button';
-    button.addEventListener('click', function() {
-        parentWindow.alert('This button has been created from an anonymous JavaScript sandbox');
-    }, false);
-    parentWindow.document.body.getElementsByTagName('p')[0].appendChild(button);
-})(parent.window || window);
-'use strict';
-(function(parentWindow) {
-    var doc, button;
-    doc = window.document;
-    button = doc.createElement('button');
+    var button;
+    button = document.createElement('button');
     button.innerHTML = 'Click here';
     button.setAttribute('type', 'button');
     button.onclick = function() {
