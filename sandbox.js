f76eab853e80c094dc097d148ba37f3324aa26b0
Michael Rouges <michael.rouges@gmail.com>
2013-03-30 02:28:43 +0100

    beautified & minified version

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,6 @@
-'use strict';
 var Sandbox;
-Sandbox = (function(window) {
+Sandbox = (function () {
+    'use strict';
     var queue, Sandbox, changeReadyState, loadSandbox;
     queue = [];
     Sandbox = function Sandbox(src, name, onReadyStateChange) {
@@ -11,13 +11,13 @@ Sandbox = (function(window) {
         this.onreadystatechange = onReadyStateChange;
         queue.push(this);
         changeReadyState(this);
-        if(queue.length === 1 && queue[0].readyState === 0) {
+        if (queue.length === 1 && queue[0].readyState === 0) {
             loadSandbox();
         }
     };
     changeReadyState = function changeReadyState(instance) {
         instance.readyState += 1;
-        if(typeof instance.onreadystatechange === 'function') {
+        if (typeof instance.onreadystatechange === 'function') {
             instance.onreadystatechange();
         }
     };
@@ -29,30 +29,30 @@ Sandbox = (function(window) {
         iframe = document.createElement('iframe');
         iframe.style.display = 'none';
         iframe.id = iframe.name = ('Sandbox__' + instance.name + new Date().getTime()) + Math.floor(Math.random() * 1000000000000000);
-        iframe.onload = function() {
+        iframe.onload = function () {
             var sandbox, document, script, onload;
-            sandbox = frames[iframe.name] || window.document.getelementById(iframe.id).contentWindow;
+            sandbox = window.frames[iframe.name] || window.document.getelementById(iframe.id).contentWindow;
             document = sandbox.document;
             script = document.createElement('script');
             script.src = instance.src;
-            onload = function() {
+            onload = function () {
                 var readyState, Window, callback;
                 readyState = this.readyState;
-                if(typeof readyState !== 'string' || (readyState === 'loaded' || readyState === 'complete')) {
+                if (typeof readyState !== 'string' || (readyState === 'loaded' || readyState === 'complete')) {
                     Window = function Window() {};
                     Window.prototype = sandbox;
                     instance.window = new Window(sandbox);
-                    if(instance.name !== void null) {
+                    if (instance.name !== void null) {
                         callback = instance.window[instance.name];
                     }
                     fragment.appendChild(iframe);
                     instance.document = document;
-                    if(typeof callback === 'function') {
+                    if (typeof callback === 'function') {
                         instance.window[instance.name] = callback;
                     }
                     changeReadyState(instance);
                     queue.shift();
-                    if(queue.length !== 0) {
+                    if (queue.length !== 0) {
                         loadSandbox();
                     }
                 }
@@ -65,4 +65,4 @@ Sandbox = (function(window) {
         changeReadyState(instance);
     };
     return Sandbox;
-})(window);
+})();
